83ed90d56199acb4fae00778432580de7c22963f
daviddev16 <davidduartepinheiro@gmail.com>
2021-08-20 08:26:07 -0300

    .

--- Assets/InventorySystem/Runtime/InventoryPrefabs.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "InventoryPrefab", menuName = "", order = -1)]
-public class InventoryPrefabs : ScriptableObject
-{
-    public GameObject DisplayedItemPrefab;
-}
